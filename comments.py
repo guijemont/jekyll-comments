f5f1a8f8e6e8c6d5cd3860da382a52be92f011bd
Guillaume Emont <guijemont@igalia.com>
2013-02-10 14:47:10 +0100

    Added copyright headers, making it official that this is distributed under the terms of the GNU Affero General Public License

--- comments.py
+++ comments.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+# Copyright 2013 Guillaume Emont <guij@emont.org>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 CGI script that takes takes a POST to the address ./add_comment (normally from
 a comment form) and sends that comment formatted in yaml to the email address
